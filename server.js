4a4a66eddbe00081b647389b2d1eb4db6a110c54
PACKSUNG PILL <rightguy22@PACKSUNGui-MacBook-Pro.local>
2020-05-22 01:01:59 +0900

    port change 8104

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(8081, function() {
-    console.log((new Date()) + ' Server is listening on port 8081');
+server.listen(8104, function() {
+    console.log((new Date()) + ' Server is listening on port 8104');
 });
 
 wsServer = new WebSocketServer({
